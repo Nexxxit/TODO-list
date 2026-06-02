213d395bd9ba2410146018a7fd37e3785478e3c3
Vans TM <varsik.arsik@mail.ru>
2026-06-03 01:15:03 +0500

    fix(server): return JSON response from tasks controller
    Adapt getTasksController to Express request/response handler and respond with an empty tasks array via res.json.

--- server/src/controllers/tasks.controller.ts
+++ server/src/controllers/tasks.controller.ts
@@ -1,5 +1,7 @@
-const getTasksController = () => {
-    return []
+import type { Request, Response } from "express"
+
+const getTasksController = (req: Request, res: Response) => {
+    return res.status(200).json([])
 }
 
 export { getTasksController }
